fe10ff8163e5e1a56ca7cc9e2619951d45db338a
Milena Figueres <figueresmilena@gmail.com>
2018-05-18 17:12:15 -0300

    Subo la vista principal (index), el model Juego y una hoja de estilos

---
+++ JuegoAdivinarNumero/JuegoAdivinarNumero/Models/Juego.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace JuegoAdivinarNumero.Models
+{
+    public abstract class Juego
+    {
+        public int numeroAleatorio { get; set; }
+        public int numeroAdivinado { get; set; }
+        public string respuesta { get; set; }
+    }
+}
